d19a32ae8e269c1fb073fdf3483d6389ac80745a
riyanpyd <riyanpyd@gmail.com>
2019-07-23 00:11:36 +0700

    TakeImageApp
    Aplikasi Camera Android

---
+++ TakeImageApp/app/src/main/java/com/example/takeimageapp/GalleryPhoto.java
@@ -0,0 +1,54 @@
+package com.example.takeimageapp;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class GalleryPhoto extends AppCompatActivity implements AdapterView.OnItemClickListener, View.OnClickListener {
+    ImageAdapter myImageAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery_photo);
+
+        GridView gridView = (GridView) findViewById(R.id.gridview);
+        myImageAdapter = new ImageAdapter(this);
+        gridView.setAdapter(myImageAdapter);
+        gridView.setOnItemClickListener(this);
+
+        String ExternalStorageDirectoryPath = Environment
+                .getExternalStorageDirectory()
+                .getAbsolutePath();
+        String targetPath = ExternalStorageDirectoryPath + "/DCIM/Camera/";
+        Toast.makeText(getApplicationContext(), targetPath, Toast.LENGTH_SHORT).show();
+        File targetDirector = new File(targetPath);
+        File[] files = targetDirector.listFiles();
+        for (File file : files) {
+            myImageAdapter.add(file.getAbsolutePath());
+        }
+    }
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(GalleryPhoto.this, "Gambar no " + position + " Dipilih", Toast.LENGTH_SHORT).show();
+        Intent i = new Intent(getApplicationContext(), SinglePhoto.class);
+        i.putExtra("path", myImageAdapter.itemList.get(position));
+        startActivity(i);
+    }
+}
---
+++ TakeImageApp/app/src/main/java/com/example/takeimageapp/ImageAdapter.java
@@ -0,0 +1,96 @@
+package com.example.takeimageapp;
+
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+    ArrayList<String> itemList = new ArrayList<String>();
+
+    public ImageAdapter(Context c)
+    {
+        mContext = c;
+    }
+
+    void add(String path){
+        itemList.add(path);
+    }
+
+    @Override
+    public int getCount() {
+        // Jumlah total gambar
+        return itemList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Mengambil satu gambar dari gallery
+        ImageView imageView;
+        if (convertView == null) {  // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(200, 200));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+        Bitmap bitmap = decodeSampledBitmapFromUri(itemList.get(position), 200, 200);
+        imageView.setImageBitmap(bitmap);
+        return imageView;
+    }
+
+    public Bitmap decodeSampledBitmapFromUri(String path, int reqWidth, int reqHeight) {
+
+        Bitmap bm = null;
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(path, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        bm = BitmapFactory.decodeFile(path, options);
+
+        return bm;
+    }
+    public int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight){
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+            if (width > height) {
+                inSampleSize = Math.round((float)height / (float)reqHeight);
+            } else {
+                inSampleSize = Math.round((float)width / (float)reqWidth);
+            }
+        }
+
+        return inSampleSize;
+    }
+}
